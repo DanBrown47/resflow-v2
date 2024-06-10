76ad460b19d0a1d0ff5e6759e608e8c736babafa
nanditha <nandithags11@gmail.com>
2024-06-10 23:23:33 +0530

    Add  File hash field

--- resflow_v2/resume/models.py
+++ resflow_v2/resume/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Resume(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
+    file_hash = models.CharField(max_length=512)
     file = models.FileField(upload_to='resumes/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.name
